c0378302da2fd76684fb6ecdd75569694761520e
Dylan Bailey <soniczentropy@gmail.com>
2017-06-26 09:15:51 -0500

    Init with backpack working

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('users')->insert([
+            'name' => 'Casey Bailey',
+            'email' => 'cbailey@pulaskitech.edu',
+            'password' => bcrypt('kckckc'),
+            'isAdmin' => 1,
+            'institution' => 'UAPTC',
+            'personal_id' => ' 116004734',
+
+        ]);
+        \DB::table('users')->insert([
+            'name' => 'Terry Patterson',
+            'email' => 'tpatterson@pulaskitech.edu',
+            'password' => bcrypt('password'),
+            'isAdmin' => 1,
+            'institution' => 'UAPTC',
+            'personal_id' => ' 787005383',
+        ]);
+        \DB::table('users')->insert([
+            'name' => 'Jasmine Williams',
+            'email' => 'jcwilliams@pulaskitech.edu',
+            'password' => bcrypt('password'),
+            'isAdmin' => 1,
+            'institution' => 'UAPTC',
+            'personal_id' => '485007588',
+        ]);
+
+        \DB::table('users')->insert([
+            'name' => 'Test User',
+            'email' => 'ofs@pulaskitech.edu',
+            'password' => bcrypt('kc'),
+            'isAdmin' => 0,
+            'institution' => 'UAPTC',
+            'personal_id' => '123456789',
+        ]);
+    }
+}
